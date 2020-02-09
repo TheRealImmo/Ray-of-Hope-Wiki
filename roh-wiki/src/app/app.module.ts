f69860ef1000339f57a9125ff8d8b491f7a84f87
TheRealImmo <adlegend@web.de>
2020-02-09 20:30:18 +0100

    Weitere Entwicklung

--- roh-wiki/src/app/app.module.ts
+++ roh-wiki/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CharactersComponent } from './characters/characters.component';
+import { EnemiesComponent } from './enemies/enemies.component';
+import { CharacterlistComponent } from './characters/characterlist/characterlist.component';
+import { EnemylistComponent } from './enemies/enemylist/enemylist.component';
+import { CharacterdetailComponent } from './characters/characterdetail/characterdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CharactersComponent,
+    EnemiesComponent,
+    CharacterlistComponent,
+    EnemylistComponent,
+    CharacterdetailComponent
   ],
   imports: [
     BrowserModule
